14ee19583502b5ef6669a3c098ea6015774443b2
ihugojimenez <hugofernando809@hotmail.com>
2016-07-09 11:21:35 -0400

    Entidades

---
+++ MainWindowsForms/Entidades/Usuarios.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Usuarios
+    {
+        public string nombreusuario { get; set; }
+        public string Contraseña { get; set; }
+        public string nombre { get; set; }
+        public string apellido  { get; set; }
+
+        public Usuarios()
+        {
+
+        }
+
+
+    }
+}
