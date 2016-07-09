7d0d52876227a9c2ab739eb25fd8ad47d39272a6
ihugojimenez <hugofernando809@hotmail.com>
2016-07-09 11:11:33 -0400

    probando...

---
+++ MainWindowsForms/DAL/AlmacenBancasDB.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class AlmacenBancasDB : DbContext
+    {
+        public AlmacenBancasDB() : base("name = hello")
+        {
+
+        }
+
+    }
+}
